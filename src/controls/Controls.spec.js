47ce69ffb9b6366e27db1dd85970626667272dbc
Joseph Graham <Joseph@Chasingthefiks.net>
2019-12-17 09:20:36 -0700

    MVP Met - All Tests report PASS

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,57 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Controls from "./Controls.js";
+
+//renders without crashing
+test("renders without crashing", () => {
+  render(<Controls />);
+});
+//render buttons
+test("render buttons closed/locked", () => {
+  const { getByText } = render(<Controls />);
+
+  getByText(/close gate/i);
+
+  getByText(/lock gate/i);
+});
+//render open
+test("render open", () => {
+  const { getByText } = render(<Controls closed={true} />);
+
+  getByText(/open gate/i);
+});
+//render unlocked
+test("render unlocked", () => {
+  const { getByText } = render(<Controls locked={true} />);
+
+  getByText(/unlock gate/i);
+});
+//close disable
+test("closed button disabled gate locked", () => {
+  const toggleClosed = jest.fn();
+
+  const { getByText } = render(
+    <Controls toggleClosed={toggleClosed} closed={true} locked={true} />
+  );
+
+  const openToggle = getByText(/open gate/i);
+
+  fireEvent.click(openToggle);
+
+  expect(toggleClosed).not.toHaveBeenCalled();
+});
+//lock disable
+test("locked button disabled - gate open", () => {
+  const toggleLocked = jest.fn();
+
+  const { getByText } = render(
+    <Controls toggleLocked={toggleLocked} closed={false} locked={false} />
+  );
+
+  const lockToggle = getByText(/lock gate/i);
+
+  fireEvent.click(lockToggle);
+
+  expect(toggleLocked).not.toHaveBeenCalled();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,32 @@
 // Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display.js";
+//renders without crashing
+test("renders without crashing", () => {
+  render(<Display />);
+});
+//renders unlocks
+test("renders unlocked", () => {
+  const { getByText } = render(<Display locked={false} />);
+
+  getByText(/^unlocked$/i);
+});
+//renders locked
+test("renders locked", () => {
+  const { getByText } = render(<Display locked={true} />);
+
+  getByText(/^locked$/i);
+});
+//renders open
+test("renders open", () => {
+  const { getByText } = render(<Display closed={false} />);
+
+  getByText(/^open$/i);
+});
+//renders closed
+test("renders closed", () => {
+  const { getByText } = render(<Display closed={true} />);
+
+  getByText(/^closed$/i);
+});
